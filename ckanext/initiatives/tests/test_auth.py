1ec103ea2eaafff1eaa07d441e88e669de5c8ff1
Mark Tearle <mark.tearle@qcif.edu.au>
2025-10-05 11:55:00 +0800

    Add tests for auth

--- ckanext/initiatives/tests/test_auth.py
+++ ckanext/initiatives/tests/test_auth.py
@@ -11,4 +11,20 @@ from ckan.common import g
 @pytest.mark.ckan_config("ckan.plugins", "initiatives")
 @pytest.mark.usefixtures("with_request_context", "with_plugins", "clean_db")
 class TestInitiativesAuth(object):
-    pass
+    def test_initiatives_resource_show_with_user(self):
+        user = factories.User()
+        # simulate logged in session
+        userobj = model.User.by_name(user["name"])
+        g.user = user["name"]
+        g.userobj = userobj
+
+        owner_org = factories.Organization(
+            users=[{"name": user["id"], "capacity": "member"}]
+        )
+        package = factories.Dataset(owner_org=owner_org["id"])
+        resource = factories.Resource(package_id=package["id"])
+        context = {"user": user["name"], "model": model, "resource": resource}
+
+        data_dict = {"id": resource["id"]}
+
+        assert test_helpers.call_auth("resource_show", context=context, data_dict=data_dict)
